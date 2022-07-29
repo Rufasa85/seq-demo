72b3deeb24291bacf5ed7f8e0bf3d299d85fda14
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-07-29 09:49:09 -0700

    basic setup

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require('express');
+
+// Import the connection object
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const Kitten = require("./models/kitten.js")
+const allRoutes = require("./controllers")
+
+app.use(allRoutes);
+
+// Connect to the database before starting the Express.js server
+sequelize.sync({force:true}).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
